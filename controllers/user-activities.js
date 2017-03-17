184d2914c997d63ec61acbd4d3ebfb1f1cadbf7c
Jacob Caban-Tomski <jcabantomski@commercialtribe.com>
2017-03-17 11:38:53 -0600

    Erics code from interview.

--- controllers/user-activities.js
+++ controllers/user-activities.js
@@ -3,3 +3,52 @@
 // - getActivityById(actId)
 // - getActivitiesByOwners(userIds)
 // - getActivitiesByOwner(userId)
+const activityService = require('../services/activity-service.js');
+const userService = require('../services/user-service.js');
+const _ = require('lodash');
+
+class userActivitiesCtrl {
+
+	static getActivityById(id) {
+		let user;
+		let foundActivity;
+		let activities = [];
+    let activitiesById = activityService.getAllByIdsQ([id]);
+
+
+		return _.map(activitiesById, (activity) => {
+			foundActivity = activity;
+			user = this.findUser(activity.ownerId)[0];
+			return {
+				activityName: foundActivity.name,
+				activityId: foundActivity.id,
+				ownerName: user.name,
+				ownerId: user.id
+			}
+		})
+	};
+
+	static getActivityByOwners(ids) {
+		let allActivities = activityService.getAllQ();
+
+		let matchedActivities = allActivities.filter(
+			(activity) => ids.indexOf(activity.ownerId) > -1
+		);
+
+		return _map(matchedActivities, activity => {
+			return this.getActivityById(activity.id)
+		});
+
+
+
+
+	};
+
+	static findUser(userId){
+		return userService.getAllByIdsQ([userId]);
+	}
+
+}
+
+module.exports = userActivitiesCtrl;
+
--- controllers/user-activities.spec.js
+++ controllers/user-activities.spec.js
@@ -1,11 +1,27 @@
+const userActivitiesCtrl = require('./user-activities');
+
 describe('user-activities', () => {
 	beforeEach(() => {
-		this.userActivitiesCtrl = require('./user-activities');
 	});
 
 	it('has getActivityById defined', () => {
-		expect(this.userActivitiesCtrl.getActivityById).toBeDefined();
+		expect(userActivitiesCtrl.getActivityById).toBeDefined();
 	});
 
+	it('returns the activity for the given id', () => {
+		expect(userActivitiesCtrl.getActivityById(1)).toEqual([
+				{
+					activityName: 'Certification',
+					activityId: 1,
+					ownerName: 'Tyler',
+					ownerId: 1
+				}
+			]
+		);
+	});
+
+	it('has getActivityByOwners defined', () => {
+		expect(userActivitiesCtrl.getActivityByOwners).toBeDefined();
+	});
 
 });
